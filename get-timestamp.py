5245ab2c044c3e48ebd68386619f3ceaec7df132
timabs <timur.abd.nyc@gmail.com>
2024-06-08 14:29:36 -0400

    update gitignore

--- get-timestamp.py
+++ get-timestamp.py
@@ -1,6 +1,7 @@
 import obsws_python as obs
 import os
 import keyboard
+import json
 from datetime import datetime
 
 # Setup for WebSocket connection to OBS
@@ -8,7 +9,23 @@ cl= obs.ReqClient(host='localhost', port=3000, password='morsey', timeout=3)
 
 # Setup for file
 timestamps_file  = 'clips.txt'
+json_file = 'date.json'
 prev_date = None
+
+
+def check_json(json_file):
+    global prev_date
+    if not os.path.exists(json_file):
+        with open(json_file, 'w') as file:
+            json.dump({'prev_date': None}, file, indent=4)
+    else:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+            prev_date = data.get('prev_date')
+def update_json_date(new_date):
+    with open (json_file, 'w') as file:
+        json.dump({'prev_date': new_date}, file, indent=4)
+
 
 def check_file_setup(file_name):
     if not os.path.exists(timestamps_file):
@@ -39,12 +56,14 @@ def write_timestamp_to_file(timestamp):
         if cur_date != prev_date:
             file.write(cur_date + '\n')
             prev_date = cur_date
+            update_json_date(cur_date)
         file.write(f'    {timestamp} \n')
 
 def get_and_write_timestamp():
     check_file_setup(timestamps_file)
     timestamp_text = get_timestamp()
     write_timestamp_to_file(timestamp_text)
+check_json(json_file)
 print(f'Press ALT + T to grab your timestamp. Press ESC to exist the program')
 keyboard.add_hotkey('alt + t', get_and_write_timestamp)
 keyboard.wait('esc')
